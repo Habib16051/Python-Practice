7b6879d7e92543f07f6264fff12202d72a6f5a17
Habib16051 <habibmhr143@gmail.com>
2024-06-06 15:59:41 +0600

    sort function added

--- tuple.py
+++ tuple.py
@@ -1,2 +1,8 @@
-numbers_tuples = (10, 20, 30, 40, 50, 60, 70, 80, 90, 100)
-print(numbers_tuples[4])
+# numbers_tuples = (10, 20, 30, 40, 50, 60, 70, 80, 90, 100)
+# print(numbers_tuples[4])
+
+guests = ['James', 'Mary', 'John', 'Patricia', 'Robert', 'Jennifer']
+
+guests.sort()
+
+print(guests)
