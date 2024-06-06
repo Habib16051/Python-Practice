ddef858b5d400c73ad6f579954465f4e3414571a
habib-technonext <habibur.rahman@technonext.com>
2024-06-06 14:12:10 +0600

    lambda function with map

--- lambda.py
+++ lambda.py
@@ -1,3 +1,11 @@
-square = lambda x : x**2
+# square = lambda x : x**2
 
-print(f'The result is : {square(5)}')
+# print(f'The result is : {square(5)}')
+
+#  A Lambda function is  python anonymous function without name; 
+# A lambda expression typically contains one or more arguments, but it can have only one expression.
+
+numbers = [1,2,3,4,5,6,7,8,9,10]
+
+result = list(map(lambda x : x ** 2, numbers))
+print(result)
