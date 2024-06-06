c561c6b3c80e1f159bf9bc41e184f18caa814d2c
habib-technonext <habibur.rahman@technonext.com>
2024-06-06 14:59:08 +0600

    list append and insert method

--- list.py
+++ list.py
@@ -1,4 +1,8 @@
 digits = [10,20,30,40,50,60,70,80]
-# digits.append(30) add at the end
-digits[0] = 50
+# digits.append(30) adding at the end of list 
+digits[0] = 50 
+
+# adding any position
+digits.insert(5,100)
+
 print(digits)
