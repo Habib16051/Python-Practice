080a339f13ba142f1dd546589b13f7c159c8decd
Habib16051 <habibmhr143@gmail.com>
2024-06-09 16:41:11 +0600

    dictionary is initialized

---
+++ dictionary.py
@@ -0,0 +1,11 @@
+person = {
+    'first_name': 'John',
+    'last_name': 'Doe',
+    'age': 25,
+    'favorite_colors': ['blue', 'green'],
+    'active': True
+}
+print(person['first_name'])
+print(person['last_name'])
+print(person['favorite_colors'])
+print(person['favorite_colors'][1])
