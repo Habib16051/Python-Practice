8082234f34ec3f1e321ab73cdbc66b2ae404ebcc
Habib16051 <habibmhr143@gmail.com>
2024-06-09 16:58:37 +0600

    dicitonary is updated with more method

--- dictionary.py
+++ dictionary.py
@@ -8,4 +8,12 @@ person = {
 print(person['first_name'])
 print(person['last_name'])
 print(person['favorite_colors'])
-print(person['favorite_colors'][1])
+print(person['favorite_colors'][1])\
+
+# using the get() method
+print(person.get('age'))
+# adding new dicitonary in the dict ==> dict [key] = value
+person['gender'] = 'male'
+#  now the new dicitonary will be like that
+print(person)
+
