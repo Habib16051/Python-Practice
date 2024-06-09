b72af1a8aa388c200fc121c1951e330c6538c775
Habib16051 <habibmhr143@gmail.com>
2024-06-09 16:06:43 +0600

    using filter method did the same thing

--- filter.py
+++ filter.py
@@ -10,3 +10,10 @@ for i in numbers:
 
 
 print(filtered)
+
+#  Now we will do the exact same thing using filter() methods
+
+result = filter(lambda x: x > 5, numbers)
+print(list(result))
+
+#  Now we will do the exact same thing using list comprehension
