c425a4bf8e164466f51a24bfe4ad4e249bd23c32
Habib16051 <habibmhr143@gmail.com>
2024-06-09 16:35:23 +0600

    list comprehension is updated

--- list_comprehension.py
+++ list_comprehension.py
@@ -1,7 +1,9 @@
+#  A basic intro of list comprehension
 numbers = [10,20,30,40,50,60,70,80,90,100]
 
 result = [number ** 2 for number in numbers]
 print(result)
 
+# when we want to add any condition in the list comprehension
 result2 = [number for number in numbers if number > 40]
 print(result2)
