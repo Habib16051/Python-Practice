acc584b4eb9166323f4f8c71a7ec0be8f99b08eb
Habib16051 <habibmhr143@gmail.com>
2024-06-10 10:08:43 +0600

    list comprehension another example

--- list_comprehension.py
+++ list_comprehension.py
@@ -7,3 +7,16 @@ print(result)
 # when we want to add any condition in the list comprehension
 result2 = [number for number in numbers if number > 40]
 print(result2)
+
+stocks = {
+    'AAPL': 121,
+    'AMZN': 3380,
+    'MSFT': 219,
+    'BIIB': 280,
+    'QDEL': 266,
+    'LVGO': 144
+}
+
+selected_stocks = {s: p for (s, p) in stocks.items() if p > 220}
+
+print(selected_stocks)
