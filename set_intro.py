ba901ba50b880fb9950b3a55c6e7bfd55371f208
Habib16051 <habibmhr143@gmail.com>
2024-06-12 11:55:39 +0600

    built in set method to create set

--- set_intro.py
+++ set_intro.py
@@ -1,2 +1,7 @@
 skills = {'Python programming','Databases', 'Software design'}
 print(skills)
+
+# You can use built in set() method to create set
+
+skills_2 = set(['Python', 'Databases', 'Software design' 'Dhaka', 'Rangpur' ])
+print(skills_2)
