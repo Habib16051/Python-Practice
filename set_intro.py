d00ccf6ac687cacf0339ea80fece27b0a9b28df2
Habib16051 <habibmhr143@gmail.com>
2024-06-12 12:15:46 +0600

    frozen set using build in method

--- set_intro.py
+++ set_intro.py
@@ -5,3 +5,20 @@ print(skills)
 
 skills_2 = set(['Python', 'Databases', 'Software design' 'Dhaka', 'Rangpur' ])
 print(skills_2)
+
+# Adding elements to a set
+skills_2.add('Java')
+
+print(skills_2)
+
+# Removing an element from a set
+skills_2.remove('Python')
+
+print(skills_2)
+
+#  You can also delete elements from a set using pop() method and discard()
+
+# Frozen sets
+skills = {'Problem solving', 'Software design', 'Python programming', 'Bangladesh'}
+skills = frozenset(skills)
+print(skills)
