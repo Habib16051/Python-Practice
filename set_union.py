16affb306de28be3be241db58e41d26fa2bd0301
Habib16051 <habibmhr143@gmail.com>
2024-06-24 14:45:32 +0600

    added another set union logic

--- set_union.py
+++ set_union.py
@@ -4,3 +4,19 @@ ranks = [2, 3, 4]
 ratings = rates.union(ranks)
 
 print(ratings)
+
+#  We can use set union another way
+
+s1 = {'Python', 'Java'}
+s2 = {'C#', 'Java'}
+
+s = s1 | s2
+
+print(s)
+
+s1 = {1,2,3,4,5 }
+s2 = {7,9,12,19,20,3}
+
+s = s1 | s2
+
+print(s)
