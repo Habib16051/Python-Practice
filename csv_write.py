cf58f9a320a287ad7ed434d96c5f0cf4df8af455
Habib16051 <habibmhr143@gmail.com>
2024-06-26 15:52:06 +0600

    csv write using python

---
+++ csv_write.py
@@ -0,0 +1,13 @@
+import csv  
+
+header = ['name', 'area', 'country_code2', 'country_code3']
+data = ['Afghanistan', 652090, 'AF', 'AFG']
+
+with open('countries.csv', 'w', encoding='UTF8') as f:
+    writer = csv.writer(f)
+
+    # write the header
+    writer.writerow(header)
+
+    # write the data
+    writer.writerow(data)
