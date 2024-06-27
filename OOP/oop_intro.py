90b0d26be59543c09d8048152630c794bd5b754e
Habib16051 <habibmhr143@gmail.com>
2024-06-27 14:28:17 +0600

    created a class name person and created instance of class and finally created object

--- OOP/oop_intro.py
+++ OOP/oop_intro.py
@@ -2,3 +2,13 @@
 # And then, from the class, you can create one or more objects. 
 # The objects are instances of a class.
 
+class Person:
+    def __init__(self, name, age):
+        self.name = name #instances of a person class
+        self.age = age #another instances of a person class
+
+result = Person('John',28) # Create a new Person object
+result2 = Person('Mst Mobasshirin Mitu', 25) # Create a new Person object
+
+print(result.name, result.age)
+print(result2.name, result2.age)
