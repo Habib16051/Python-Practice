715b49238dbc258d8db3927ba1b7681f4f3bff8a
Habib16051 <habibmhr143@gmail.com>
2024-06-27 13:30:12 +0600

    oop initialized

---
+++ OOP/oop_intro.py
@@ -0,0 +1,4 @@
+# An object has a state and behaviors. To create an object, you define a class first. 
+# And then, from the class, you can create one or more objects. 
+# The objects are instances of a class.
+
