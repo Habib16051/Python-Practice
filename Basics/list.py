a8e0e38a88c97e76674c3de3c09371ff37457ff1
Md Habibur Rahman <39822204+Habib16051@users.noreply.github.com>
2025-11-10 12:44:04 +0600

    Update list.py

--- Basics/list.py
+++ Basics/list.py
@@ -1,3 +1,11 @@
+What is a List?
+-------------------------------------------------------------------------------------------
+A list in Python is a collection of things (elements) stored in a particular order. The key points:
+The elements inside a list can be of different types (e.g., strings, numbers, booleans, even other lists) all together. 
+The list remembers the order of its elements (so the first element keeps index 0, next index 1, etc.). 
+You can access elements by their index. Also negative indexing works (from the end) in Python lists. 
+Lists are dynamic: you can change them after creating them (add, remove, modify elements).
+--------------------------------------------------------------------------------------------
 digits = [10,20,30,40,50,60,70,80]
 # adding at the end of list 
 digits.append(30) 
